6715df84e66546a0f5c443965a5b90eae1b59e6d
tertiomodo <itertiomodo@gmail.com>
2023-02-12 22:49:06 +0300

    feat: Implemented mobile adaptive layout

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# profit-kit
+<h1>Тестовое задание компании "ProfitKit"</h1>
+<h2>
+  Deploy: <a href="https://tertiomodo.github.io/profit-kit">GitHub pages</a>
+</h2>
+<h2>Используемые технологии</h2>
+<ul>
+  <li>HTML</li>
+  <li>SCSS</li>
+  <li>JavaScript</li>
+</ul>
+<h2>Общий вид проекта</h2>
+<div style="margin-bottom: 40px;">
+<img src="assets\img\readme-img\project.jpg">
+</div>
+<h2>Особенности реализации</h2>
+<div style="margin-bottom: 40px;">
+<p>Активные табы с разным контентом:</p>
+<img src="assets\img\readme-img\second-page.jpg">
+<img src="assets\img\readme-img\third-page.jpg">
+</div>
+<div style="margin-bottom: 40px;">
+<p>Слайдер для изображений в карточках:</p>
+<img src="assets\img\readme-img\img-slaider.jpg">
+</div>
+<div style="margin-bottom: 40px;">
+<p>Счетчик количества товаров:</p>
+<img src="assets\img\readme-img\counter.jpg">
+</div>
+<div style="margin-bottom: 40px;">
+<p>Адаптив под мобильную версию:</p>
+<img src="assets\img\readme-img\mobile-version.jpg">
+</div>
--- js/main.js
+++ js/main.js
@@ -1,28 +1,39 @@
+// Sliders ----------------------------------------------------
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 4,
-  slidesPerGroup: 4,
-  spaceBetween: 17,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 1,
   speed: 800,
-  loop: true,
+  loop: false,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   allowTouchMove: false,
-//   breakpoints: {
-//     768: {
-//         slidesPerView: 'auto',
-//         spaceBetween: 8
-//     },
-//     991: {
-//         slidesPerView: 2,
-//         spaceBetween: 30
-//     },
-//     1192: {
-//         slidesPerView: 3,
-//         spaceBetween: 20
-//     }
-// }
+  breakpoints: {
+    300: {
+      allowTouchMove: true,
+      slidesPerView: 1.05,
+      slidesPerGroup: 1,
+      spaceBetween: 10,
+    },
+    730: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 10,
+    },
+    1100: {
+      allowTouchMove: false,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 14,
+    },
+    1440: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 16,
+    },
+  }
 });
 
 new Swiper('.swiper-img', {
@@ -35,4 +46,124 @@ new Swiper('.swiper-img', {
     type: 'bullets',
     clickable: true,
   },
+});
+
+// Like button listener ----------------------------------------------------
+const likeBtn = document.querySelectorAll('.like-interaction-btn');
+
+likeBtn.forEach(e => {
+  e.addEventListener('click', element => {
+    const target = element.currentTarget;
+    target.children[0].classList.toggle('like-active');
+  })
+});
+
+// Counter listener ----------------------------------------------------
+const counterWrap = document.querySelectorAll('.card__buttons-wrap');
+
+counterWrap.forEach(element => {
+  const btnMinus = element.querySelector('.btn-minus');
+  const btnPlus = element.querySelector('.btn-plus');
+  const counter = element.querySelector('.card__counter-number');
+  const buyButton = element.querySelector('.card__button');
+
+  btnMinus.addEventListener('click', () => {
+    let number = +counter.textContent;
+    if (number === 1) return;
+    counter.textContent = --number;
+  })
+
+  btnPlus.addEventListener('click', () => {
+    let number = +counter.textContent;
+    counter.textContent = ++number;
+  })
+
+  buyButton.addEventListener('click', () => {
+    counter.textContent = 1;
+  })
+});
+
+// Card swap ----------------------------------------------------
+const cards = document.querySelectorAll('.card');
+const headerLink = document.querySelectorAll('.header__item-link');
+const popular = document.querySelector('.link-popular');
+const recently = document.querySelector('.link-recently');
+const interesting = document.querySelector('.link-interesting');
+const like = document.querySelectorAll('.like-interaction-btn');
+const count = document.querySelectorAll('.card__counter-number');
+
+recently.addEventListener('click', () => {
+  headerLink.forEach(e => {
+    e.classList.remove('link-active');
+    recently.classList.add('link-active');
+  })
+
+  count.forEach(e => {
+    e.textContent = 1;
+  })
+
+  like.forEach(e => {
+    e.children[0].classList.remove('like-active');
+  })
+
+  cards.forEach(element => {
+    const cardImg = element.querySelectorAll('.card__top-img');
+    const cardDescr = element.querySelector('.card__product-name');
+    const cardPrice = element.querySelector('.card__price');
+    const cardTag = element.querySelectorAll('.card__top-tag');
+    const oldPrice = element.querySelectorAll('.card__old-price');
+
+    cardImg.forEach(e => {
+      e.setAttribute('src', './assets/img/watch.jpg');
+      e.setAttribute('alt', 'Умные часы SMARTWATCH XM 109 черный');
+    })
+    cardDescr.textContent = 'Умные часы SMARTWATCH XM 109 черный';
+    cardPrice.textContent = '12 999 ₽';
+    cardTag.forEach(e => {
+      e.remove();
+    })
+    oldPrice.forEach(e => {
+      e.remove();
+    })
+  });
+});
+
+interesting.addEventListener('click', () => {
+  headerLink.forEach(e => {
+    e.classList.remove('link-active');
+    interesting.classList.add('link-active');
+  })
+
+  count.forEach(e => {
+    e.textContent = 1;
+  })
+
+  like.forEach(e => {
+    e.children[0].classList.remove('like-active');
+  })
+
+  cards.forEach(element => {
+    const cardImg = element.querySelectorAll('.card__top-img');
+    const cardDescr = element.querySelector('.card__product-name');
+    const cardPrice = element.querySelector('.card__price');
+    const cardTag = element.querySelectorAll('.card__top-tag');
+    const oldPrice = element.querySelectorAll('.card__old-price');
+
+    cardImg.forEach(e => {
+      e.setAttribute('src', './assets/img/plant.jpg');
+      e.setAttribute('alt', 'Кашпо для цветов керамическое СИМПЛГРЕЙ');
+    })
+    cardDescr.textContent = 'Кашпо для цветов керамическое СИМПЛГРЕЙ';
+    cardPrice.textContent = '450 ₽';
+    cardTag.forEach(e => {
+      e.remove();
+    })
+    oldPrice.forEach(e => {
+      e.remove();
+    })
+  });
+});
+
+popular.addEventListener('click', () => {
+  window.location.reload();
 });
